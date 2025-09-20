6a1e254f7bbef159de6f753ff043804f1beb62e7
Yunyao <yaoyl753951@gmail.com>
2025-09-20 21:30:52 +0800

    刷新域名

--- zh/js/bundle.3e92faa48831b836cbebaabc5f019e9aaff62c39da4af0af826016f60de58a56dc0c0021fc8b8dc8f6704c87c2bf554bba6bd543e5ecf4d19995f3cdbd56ce37.js
+++ zh/js/bundle.3e92faa48831b836cbebaabc5f019e9aaff62c39da4af0af826016f60de58a56dc0c0021fc8b8dc8f6704c87c2bf554bba6bd543e5ecf4d19995f3cdbd56ce37.js
@@ -10,7 +10,7 @@ const imageAltClass = 'img_alt';
 
 
 const defaultSiteLanguage = 'en';
-const baseURL = 'https://yunyaolong.github.io/longyunyao.github.io/';
+const baseURL = 'https://yunyaolong.github.io/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
